4de271c6c48d424be5ab8ae95a1b9cc5476cefef
Khushboo Kumari <khukumar@WKMZT08E4BCC.global.publicisgroupe.net>
2022-01-27 18:29:20 +0530

    services fixes ad readme updates

--- services/index.js
+++ services/index.js
@@ -6,10 +6,14 @@
 
 'use strict';
 
-const componentExists = require('../utils/componentExists');
-const config = require('../constants');
+import componentExists from '../utils/componentExists.js'
+import monorepoQues from '../utils/monorepoHelpers.js'
+import config from '../constants.js'
+import {getComputedFolderPath, getFileExtension} from '../utils/common.js'
 
-module.exports = {
+const fileExtension = getFileExtension()
+
+export default {
   description: 'Add a service',
   prompts: [
     {
@@ -37,12 +41,13 @@ module.exports = {
   ],
   actions: (data) => {
     // Generate serviceName.js 
-    const folderPath =  `../../${config.SERVICES}/${config.API_SRC}`;
+    const folderPath =  `../../${getComputedFolderPath(config.SERVICES, config.API_SRC)}`
+    
     const actions = [
       {
         type: 'add',
-        path: `${folderPath}/{{properCase name}}Service.js`,
-        templateFile: data.isGraphql == 'yes'? './services/js-templates/graphql.js.hbs' : './services/js-templates/index.js.hbs',
+        path: `${folderPath}/{{properCase name}}Service.${fileExtension}`,
+        templateFile: data.isGraphql == 'yes'? `./services/${fileExtension}-templates/graphql.${fileExtension}.hbs` : `./services/${fileExtension}-templates/index.${fileExtension}.hbs`,
         abortOnFail: true
       }
      
